179b22a617d2fc8bc5e00876c932c006a4ab182a
galewarning <93493874+galewarning@users.noreply.github.com>
2023-09-25 23:33:02 -0400

    create title and icons for sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {IoReorderThreeOutline} from 'react-icons/io5'
+import { mainu } from './SidebarConfig'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+        <div>
+            <div>
+                <img className='w-40' src="https://i.imgur.com/zqpwkLQ.png" alt="" />
+            </div>
+            <div className='w-10'>
+                {mainu.map((item => <div className='flex items-center mb-5 cursor-pointer text-lg'>
+                    <p>{item.title}</p>
+                    {item.icon}
+                </div>))}
+                
+                
+            </div>
+        </div>
+        <div className='flex items-center cursor-pointer'>
+            <IoReorderThreeOutline />
+            <p className='ml-5'>More</p>
+        </div>
+    </div>
   )
 }
 
---
+++ src/Components/Sidebar/SidebarConfig.js
@@ -0,0 +1,46 @@
+import {AiFillCompass, AiFillHeart, AiFillHome, AiFillMessage, AiFillPlusCircle, AiOutlineCompass, AiOutlineHeart, AiOutlineHome, AiOutlineMessage, AiOutlinePlusCircle, AiOutlineSearch} from 'react-icons/ai'
+import {RiVideoFill, RiVideoLine} from 'react-icons/ri'
+import {CgProfile} from 'react-icons/cg'
+
+export const mainu = [
+    {
+        title: "Home", 
+        icon: <AiOutlineHome></AiOutlineHome>, 
+        activeIcon: <AiFillHome></AiFillHome>
+    },  
+    {
+        title: "Search", 
+        icon: <AiOutlineSearch></AiOutlineSearch>, 
+        activeIcon: <AiOutlineSearch></AiOutlineSearch>
+    },
+    {
+        title: "Explore", 
+        icon: <AiOutlineCompass></AiOutlineCompass>, 
+        activeIcon: <AiFillCompass></AiFillCompass>
+    },
+    {
+        title: "Reels", 
+        icon: <RiVideoLine></RiVideoLine>, 
+        activeIcon: <RiVideoFill></RiVideoFill>
+    },
+    {
+        title: "Message", 
+        icon: <AiOutlineMessage></AiOutlineMessage>, 
+        activeIcon: <AiFillMessage></AiFillMessage>
+    }, 
+    {
+        title: "Notification",
+        icon: <AiOutlineHeart></AiOutlineHeart>,
+        activeIcon: <AiFillHeart></AiFillHeart>
+    },
+    {
+        title: "Create",
+        icon: <AiOutlinePlusCircle></AiOutlinePlusCircle>,
+        activeIcon: <AiFillPlusCircle></AiFillPlusCircle>
+    },
+    {
+        title: "Profile",
+        icon: <CgProfile></CgProfile>,
+        activeIcon: <CgProfile></CgProfile>
+    }
+]
