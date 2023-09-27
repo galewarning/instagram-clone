97c13cedd58698cc87b7a96c25d5d6183e986626
galewarning <93493874+galewarning@users.noreply.github.com>
2023-09-26 20:55:29 -0400

    modify the layout of sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -4,23 +4,23 @@ import { mainu } from './SidebarConfig'
 
 const Sidebar = () => {
   return (
-    <div>
-        <div>
+    <div className='sticky top-0 h-[100vh]'>
+        <div className='flex flex-col justify-between h-full'>
             <div>
-                <img className='w-40' src="https://i.imgur.com/zqpwkLQ.png" alt="" />
+                <div className='pt-10'>
+                    <img className='w-40' src="https://i.imgur.com/zqpwkLQ.png" alt="" />
+                </div>
+                <div className='mt-10'>
+                    {mainu.map((item => <div className='flex items-center mb-5 cursor-pointer text-lg'>
+                        {item.icon}
+                        <p>{item.title}</p>
+                    </div>))}
+                </div>
             </div>
-            <div className='w-10'>
-                {mainu.map((item => <div className='flex items-center mb-5 cursor-pointer text-lg'>
-                    <p>{item.title}</p>
-                    {item.icon}
-                </div>))}
-                
-                
+            <div className='flex items-center cursor-pointer pb-10'>
+                <IoReorderThreeOutline />
+                <p className='ml-5'>More</p>
             </div>
-        </div>
-        <div className='flex items-center cursor-pointer'>
-            <IoReorderThreeOutline />
-            <p className='ml-5'>More</p>
         </div>
     </div>
   )
