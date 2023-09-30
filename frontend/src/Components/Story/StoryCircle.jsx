d40aa4c12f3a80db42ca5f6c208d670156a4ec51
galewarning <93493874+galewarning@users.noreply.github.com>
2023-09-30 16:35:28 -0400

    move the frontend work to frontend folder

---
+++ frontend/src/Components/Story/StoryCircle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const StoryCircle = () => {
+	return (
+		<div>
+			<img
+				className="w-16 h-16 rounded-full"
+				src="https://cdn.pixabay.com/photo/2023/04/28/19/12/stream-7957258_1280.jpg"
+				alt=""
+			/>
+			<p>username</p>
+		</div>
+	);
+};
+
+export default StoryCircle;
