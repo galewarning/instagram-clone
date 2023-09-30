36989ee63fcfb08bb650c06b6c1f084afd30d8c2
galewarning <93493874+galewarning@users.noreply.github.com>
2023-09-29 20:12:28 -0400

    format with prettier

--- src/Pages/Router/Router.jsx
+++ src/Pages/Router/Router.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from '../HomePage/HomePage'
+import React from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../HomePage/HomePage";
 
 const Router = () => {
   return (
     <div>
-        <div className='flex'>
-            <div className='w-[20%] border border-1-slate-500'>
-                <Sidebar/>
-            </div>
-            <div>
-                <Routes>
-                    <Route path='/' element={<HomePage/>}></Route>
-                </Routes>
-            </div>
+      <div className="flex">
+        <div className="w-[20%] border border-1-slate-500">
+          <Sidebar />
         </div>
+        <div>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+          </Routes>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
