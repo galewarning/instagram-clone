68ff3f72ea797f7493fb9e978c002aa4206563ac
galewarning <93493874+galewarning@users.noreply.github.com>
2023-10-02 17:34:22 -0400

    add UI for comment and posts metadata

--- frontend/src/Components/Post/PostCard.jsx
+++ frontend/src/Components/Post/PostCard.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { BsBookmark, BsBookmarkFill, BsThreeDots } from "react-icons/bs";
+import {
+	BsBookmark,
+	BsBookmarkFill,
+	BsEmojiSmile,
+	BsThreeDots,
+} from "react-icons/bs";
 import "./PostCard.css";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { FaRegComment } from "react-icons/fa";
@@ -90,6 +95,24 @@ const PostCard = () => {
 								onClick={handleSavePost}
 							/>
 						)}
+					</div>
+				</div>
+
+				<div className="w-full py-2 px-5">
+					<p>10 likes</p>
+					<p className="opacity-50 py-2 cursor-pointer">
+						view all 10 comments
+					</p>
+				</div>
+
+				<div className="border border-t w-full">
+					<div className="flex w-full items-center px-5">
+						<BsEmojiSmile />
+						<input
+							className="commentInput"
+							type="text"
+							placeholder="Add a comment..."
+						/>
 					</div>
 				</div>
 			</div>
